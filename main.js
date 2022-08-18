52d52458edb8a5169cdcecdc54bf7918a4bb8d45
szilviabarath <szilvia.barat@gmail.com>
2022-08-18 19:48:23 +0200

    changes

--- main.js
+++ main.js
@@ -53,3 +53,11 @@ copies.forEach(c => {
 
 
 document.querySelector("#letters").style.display = "block";
+
+//spot overflow//
+document.querySelectorAll('*').forEach(el => {
+    if (el.offsetWidth > document.documentElement.offsetWidth) {
+        el.style.border = '1px solid red';
+        console.error('Overflowing element:', el);
+    }
+  });
