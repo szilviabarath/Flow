509dc0105b21860dc1f87a921b644db6fcad0b63
szilviabarath <szilvia.barat@gmail.com>
2022-09-14 19:04:16 +0200

    changes

--- flow-css/overflow.js
+++ flow-css/overflow.js
@@ -4,3 +4,15 @@ document.querySelectorAll('*').forEach(el => {
         console.error('Overflowing element:', el);
     }
   });
+
+  //fix overflow for viewport units//
+function handleFullWidthSizing(el) {
+
+    const scrollbarWidth = window.innerWidth - document.body.clientWidth
+    var els = document.querySelectorAll('*');
+    for(var i=0; i < els.length;i++) {
+        document.querySelector(el).style.width = `calc(100vw - ${scrollbarWidth}px)`
+    }
+}
+//replace Element with your element's name//
+handleFullWidthSizing("Element");
